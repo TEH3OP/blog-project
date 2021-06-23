9fe32a9b7a1344b0664e19e50110420237209a13
Hryhorii <sagram.net@gmail.com>
2021-06-23 19:46:51 +0300

    Added js with nav menu

--- app.js
+++ app.js
@@ -43,3 +43,18 @@ function showModalByScroll () {
 
 window.addEventListener("scroll", showModalByScroll);
 
+let mobileMenu = document.querySelector(".nav-mobile-menu");
+let mainMenu = document.querySelector(".navigation");
+
+// console.log(mobileMenu);
+// console.log(mainMenu);
+
+mobileMenu.addEventListener("click", function(){
+    mobileMenu.classList.toggle("active-menu");
+    if (mobileMenu.classList.contains("active-menu")){
+        mainMenu.classList.add("active-menu");
+    } else {
+        mainMenu.classList.remove("active-menu");
+    }
+});
+
